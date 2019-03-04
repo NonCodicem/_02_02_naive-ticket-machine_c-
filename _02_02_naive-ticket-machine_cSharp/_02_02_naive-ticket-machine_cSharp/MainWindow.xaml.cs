8574172e2e85c2dea8ec33b26421f6c9110b0019
Serge Fabre <serge.fabre@intecbrussel.be>
2019-03-04 19:31:40 +0100

    Half way

--- _02_02_naive-ticket-machine_cSharp/_02_02_naive-ticket-machine_cSharp/MainWindow.xaml.cs
+++ _02_02_naive-ticket-machine_cSharp/_02_02_naive-ticket-machine_cSharp/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace _02_02_naive_ticket_machine_cSharp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int cost { get; set; }
+        public int balance { get; set; }
+        public int total { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            cost = 100;
+            
+            lbl_ticketCost.Content = $"Each ticket costs {cost} .";
+            txt_insertedAmount.Text = $"Insert money";
         }
+
+                
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            balance += Convert.ToInt32(txt_insertedAmount.Text);
+            lbl_balance.Content = balance;
+            txt_insertedAmount.Text = $"Insert {cost - balance} € extra";
+        }
+
+        
+
+        private void btn_reset_Click(object sender, RoutedEventArgs e)
+        {
+            lbl_balance.Content = balance = 0;
+            txt_insertedAmount.Text = $"Insert money";
+        }
+
+        private void txt_insertedAmount_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txt_insertedAmount.Text = "";
+        }
+
+       
     }
 }
