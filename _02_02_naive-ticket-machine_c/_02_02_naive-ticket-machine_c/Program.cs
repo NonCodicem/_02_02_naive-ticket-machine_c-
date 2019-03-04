d9f9d21fc6d15a66b1ea0f31cc450b75e27c3f75
Serge Fabre <serge.fabre@intecbrussel.be>
2019-03-04 12:58:34 +0100

    upload solution

---
+++ _02_02_naive-ticket-machine_c/_02_02_naive-ticket-machine_c/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_02_naive_ticket_machine_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
