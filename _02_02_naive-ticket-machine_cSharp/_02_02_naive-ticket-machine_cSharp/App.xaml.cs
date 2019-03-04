729d0a080c1d5cfb3680bee28d4279863078dd0b
Serge Fabre <serge.fabre@intecbrussel.be>
2019-03-04 14:27:40 +0100

    WPF

---
+++ _02_02_naive-ticket-machine_cSharp/_02_02_naive-ticket-machine_cSharp/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _02_02_naive_ticket_machine_cSharp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
